6099460e1080a11912e29cc8dca27c6b5e11d02a
shahriyar <mdshahriyaralam552@gmail.com>
2022-03-22 16:01:33 +0600

    Move components to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Products from "./Products";
+import Products from "./components/Products";
 
 const App = () => {
   const [cart, setCart] = useState([]);
